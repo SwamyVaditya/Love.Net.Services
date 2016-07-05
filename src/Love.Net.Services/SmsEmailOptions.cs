b0bc0a56985645f9d728036bf400764032319e25
RigoFunc <rigofunc@outlook.com>
2016-07-05 16:09:22 +0800

    feat: Initial commit

---
+++ src/Love.Net.Services/SmsEmailOptions.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) rigofunc and lotosbin. All rights reserved.
+
+namespace Love.Net.Services {
+    /// <summary>
+    /// Represents all the options you can user to configure the service.
+    /// </summary>
+    public class SmsEmailOptions {
+        /// <summary>
+        /// Gets or sets the send Sms API URL.
+        /// </summary>
+        /// <value>The send Sms API URL.</value>
+        public string SmsApiUrl { get; set; }
+        /// <summary>
+        /// Gets or sets the send email API URL.
+        /// </summary>
+        /// <value>The send email API URL.</value>
+        public string EmailApiUrl { get; set; }
+    }
+}
